7c83c1d63765ff83171fcffabf09c184e35d5a07
Bruno Liberini <bliberini@Brunos-MacBook-Air.local>
2018-04-06 23:27:16 -0300

    Cambio velocidad de mostrar mensaje de error del server

--- src/webapp/app/components/login/loginService.js
+++ src/webapp/app/components/login/loginService.js
@@ -2,7 +2,7 @@ app.service('loginService', function($http) {
     var login = function (credentials) {
         return $http({
                 method: 'POST',
-                url: 'http://localhost:5000/api/login',
+                url: 'http://localhost:8080/api/login',
                 headers: {
                     'Content-Type': 'application/json'
                 },
